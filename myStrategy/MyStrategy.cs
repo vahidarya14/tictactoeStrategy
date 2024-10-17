816b4e056bdeecee8a9f0936c2a98066edc15c4e
v.arya <v.arya@ARIYA-LAPTOP>
2024-10-17 12:04:29 +0330

    Add project files.

---
+++ myStrategy/MyStrategy.cs
@@ -0,0 +1,43 @@
+﻿
+using A.Framework;
+
+namespace myStrategy;
+
+public class MyStrategy(Board board) : PcStrategy(board)
+{
+    public override string Name => "my Strategy name";
+
+    public override async ValueTask PcMove(bool asStart = false)
+    {
+        //1-this is how you have a property witch is [ground), to access all cells
+        var topLeft = Cell(Position.topLeft);         //equals Cell(0,0)
+        var topMidle = Cell(Position.topMidle);       //equals Cell(0,1)
+        var topright = Cell(Position.topright);       //equals Cell(0,2)
+        var midleLeft = Cell(Position.midleLeft);     //equals Cell(1,0)
+        var midlle = Cell(Position.midlle);           //equals Cell(1,1)
+        var midleright = Cell(Position.midleright);   //equals Cell(1,2)
+        var bottomLeft = Cell(Position.bottomLeft);   //equals Cell(2,0)
+        var bottomMidle = Cell(Position.bottomMidle); //equals Cell(2,1)
+        var bottomRight = Cell(Position.bottomRight); //equals Cell(2,2)
+
+
+        if (topLeft == (int)Turn.Pc) ;     //<== meas PC have mark cell topLeft (buy using your  algorithm)
+        if (topLeft == (int)Turn.Player) ; //<== meas your competitor have mark cell topLeft
+
+
+
+        //2-this is howto put you mark in selected cell use one of these 
+        await MyChoiceIs(0, 2);
+        //or
+        await MyChoiceIs(Position.midleLeft);
+
+
+        //3-sample 
+        //write our  algorithm here. something like this
+        if (topLeft == (int)Turn.Pc && topright == (int)Turn.Pc)
+        {
+            await MyChoiceIs(Position.topMidle);
+        }
+
+    }
+}
---
+++ myStrategy/PcStrategy3.cs
@@ -0,0 +1,79 @@
+﻿using A.Framework;
+
+namespace board.Startegy2;
+
+public class PcStrategy3(Board board) : PcStrategy(board)
+{
+    public override string Name { get; } = "vahid-Easy";
+
+    public override async ValueTask PcMove(bool asStart = false)
+    {
+        var lst = new int?[3];
+        #region  //--------------------------first check my win situation
+        for (int w = 0; w < 3; w++)
+        {
+            lst = [Cell(w, 0), Cell(w, 1), Cell(w, 2)];
+            if (lst.Count(x => x.HasValue && x.Value == (int)Turn.Pc) == 2)
+            {
+                for (var room = 0; room < lst.Length; room++)
+                    if (!Cell(w, room).HasValue)
+                    {
+                        await  MyChoiceIs(w, room);
+                        return;
+                    }
+            }
+        }
+        for (int w = 0; w < 3; w++)
+        {
+            lst = [Cell(0, w), Cell(1, w), Cell(2, w)];
+            if (lst.Count(x => x.HasValue && x.Value == (int)Turn.Pc) == 2)
+            {
+                for (var room = 0; room < lst.Length; room++)
+                    if (!Cell(room, w).HasValue)
+                    {
+                        await  MyChoiceIs(room, w);
+
+                        return;
+                    }
+            }
+        }
+
+        lst = [Cell(0, 0), Cell(1, 1), Cell(2, 2)];
+        if (lst.Count(x => x.HasValue && x.Value == (int)Turn.Pc) == 2)
+        {
+            for (var w = 0; w < lst.Length; w++)
+                if (!Cell(w, w).HasValue)
+                {
+                    await  MyChoiceIs(w, w);
+                    return;
+                }
+        }
+
+        lst = [Cell(0, 2), Cell(1, 1), Cell(2, 0)];
+        if (lst.Count(x => x.HasValue && x.Value == (int)Turn.Pc) == 2)
+        {
+            for (var w = 0; w < lst.Length; w++)
+                if ((w == 0 && Cell(0, 2) == null) || (w == 1 && Cell(1, 1) == null) || (w == 2 && Cell(2, 0) == null))
+                {
+                    if (w == 0 && Cell(0, 2) == null) await MyChoiceIs(0, 2);
+                    if (w == 1 && Cell(1, 1) == null) await MyChoiceIs(1, 1);
+                    if (w == 2 && Cell(2, 0) == null) await MyChoiceIs(2, 0);
+                    return;
+                }
+        }
+        #endregion
+
+
+        while (true)
+        {
+            var aa = new Random().Next(0, 9);
+            var i = aa < 3 ? 0 : aa < 6 ? 1 : 2;
+            var j = aa % 3;
+            if (Cell(i, j) == null)
+            {
+                await MyChoiceIs(i, j);
+                break;
+            }
+        }
+    }
+}
